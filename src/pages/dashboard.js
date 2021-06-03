948ca7e11d497da6b898152d5faf42582087624d
DG <dg@email.com>
2021-06-03 16:38:43 +0530

    data from airtable

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 import Wrapper from '../components/Wrapper'
 import SEO from '../components/SEO'
 import Moment from 'react-moment'
+import moment from 'moment'
 import styled from 'styled-components'
 
 const Heading = styled.h1`
@@ -52,6 +53,72 @@ const Data = styled.h2`
 `
 
 const Dashboard = (props) => {
+
+  // console.log("******* process.env.GATSBY_AIRTABLE_API_KEY",process.env.GATSBY_AIRTABLE_API_KEY)
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: process.env.GATSBY_AIRTABLE_API_KEY}).base('appARM4wlkiCLYVKH');
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    base('Tracking').select({
+      maxRecords: 2,
+      view: "Grid view",
+      sort: [{field: "SrNo", direction: "desc"}]
+    }).eachPage(function page(records, fetchNextPage) {
+      let currentData = {}
+      let previousData = {}
+      let first = true;
+      records.forEach(function (record) {        
+        // const recordDate = record.get('RecordDate');
+        // const recordDateOnly = moment(recordDate).format('YYYY-MM-DD')
+        // console.log('Retrieved', record.get('SrNo'),record.get('Total'));
+        if (first) {
+          currentData = {
+            total: record.get('Total'),
+            binance: record.get('Binance-Spot')+record.get('Binance-Earn'),
+            swissborg: record.get('SwissBorg'),
+            trustwallet: record.get('TrustWallet'),
+          }
+          first = false;
+        } else {
+          previousData = {
+            total: record.get('Total'),
+            binance: record.get('Binance-Spot')+record.get('Binance-Earn'),
+            swissborg: record.get('SwissBorg'),
+            trustwallet: record.get('TrustWallet'),
+          }
+        }
+      });
+      setData({
+        total: {
+          current: currentData.total, 
+          diffAmount: (currentData.total - previousData.total).toFixed(2),
+          percent: ((currentData.total - previousData.total)*100/previousData.total).toFixed(2),
+        },
+        binance: {
+          current: currentData.binance, 
+          diffAmount: (currentData.binance - previousData.binance).toFixed(2),
+          percent: ((currentData.binance - previousData.binance)*100/previousData.binance).toFixed(2),
+        },
+        swissborg: {
+          current: currentData.swissborg, 
+          diffAmount: (currentData.swissborg - previousData.swissborg).toFixed(2),
+          percent: ((currentData.swissborg - previousData.swissborg)*100/previousData.swissborg).toFixed(2),
+        },
+        trustwallet: {
+          current: currentData.trustwallet, 
+          diffAmount: (currentData.trustwallet - previousData.trustwallet).toFixed(2),
+          percent: ((currentData.trustwallet - previousData.trustwallet)*100/previousData.trustwallet).toFixed(2),
+        },
+      })
+      fetchNextPage();
+    }, function done(err) {
+      if (err) { console.error(err); return; }
+    });
+  }, []);
+  
+
     return (
       <Layout location={props.location}>
         <SEO />
@@ -60,23 +127,23 @@ const Dashboard = (props) => {
           <DashWrapper>
           <Box>
             <BoxTitle>Total</BoxTitle>
-            <BoxAmount>1050.00</BoxAmount>
-            <Data className="up">13.72 <div>(2.67%)</div></Data>
+            <BoxAmount>{data && data.total.current}</BoxAmount>
+            <Data className={data && data.total.diffAmount>0?'up':'down'}>{data && data.total.diffAmount} <div>({data && data.total.percent}%)</div></Data>
           </Box>
           <Box>
             <BoxTitle>Binance</BoxTitle>
-            <BoxAmount>1050.00</BoxAmount>
-            <Data className="up">13.72 <div>(2.67%)</div></Data>
+            <BoxAmount>{data && data.binance.current}</BoxAmount>
+            <Data className={data && data.binance.diffAmount>0?'up':'down'}>{data && data.binance.diffAmount} <div>({data && data.binance.percent}%)</div></Data>
           </Box>
           <Box>
             <BoxTitle>SwissBorg</BoxTitle>
-            <BoxAmount>1050.00</BoxAmount>
-            <Data className="up">13.72 <div>(2.67%)</div></Data>
+            <BoxAmount>{data && data.swissborg.current}</BoxAmount>
+            <Data className={data && data.swissborg.diffAmount>0?'up':'down'}>{data && data.swissborg.diffAmount} <div>({data && data.swissborg.percent}%)</div></Data>
           </Box>
           <Box>
             <BoxTitle>TrustWallet</BoxTitle>
-            <BoxAmount>1050.00</BoxAmount>
-            <Data className="up">13.72 <div>(2.67%)</div></Data>
+            <BoxAmount>{data && data.trustwallet.current}</BoxAmount>
+            <Data className={data && data.trustwallet.diffAmount>0?'up':'down'}>{data && data.trustwallet.diffAmount} <div>({data && data.trustwallet.percent}%)</div></Data>
           </Box>
           </DashWrapper>
         </Wrapper>
